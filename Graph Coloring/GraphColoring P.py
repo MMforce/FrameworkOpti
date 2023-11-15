2d15b10f5454c49c84621cfead1d11717a872517
Matías Mora <128401777+MMforce@users.noreply.github.com>
2023-11-15 19:39:07 -0300

    Update GraphColoring P.py

--- Graph Coloring/GraphColoring P.py
+++ Graph Coloring/GraphColoring P.py
@@ -6,32 +6,32 @@ class Grafo:
     def __init__(self, edges, n):
         self.adjList = [[] for _ in range(n)]
 
-        # agrega bordes al grafo no dirigido
+        #agrega bordes al grafo no dirigido
         for (src, dest) in edges:
             self.adjList[src].append(dest)
             self.adjList[dest].append(src)
 
 
-# Función para asignar colores a los vértices de un grafo
+#Función para asignar colores a los vértices de un grafo
 def colorGrafo(grafo, n):
 
-    # realiza un seguimiento del color asignado a cada vértice
+    #realiza un seguimiento del color asignado a cada vértice
     resultado = {}
 
-    # asigna un color a los vértices uno por uno
+    #asigna un color a los vértices uno por uno
     for u in range(n):
 
-        # verifica los colores de los vértices adyacentes de `u` y los almacena en un conjunto
+        #verifica los colores de los vértices adyacentes de `u` y los almacena en un conjunto
         asignacion = set([resultado.get(i) for i in grafo.adjList[u] if i in resultado])
 
-        # comprueba el primer color libre
+        #comprueba el primer color libre
         color = 1
         for c in asignacion:
             if color != c:
                 break
             color = color + 1
 
-        # asigna al vértice u el primer color disponible
+        #asigna al vértice u el primer color disponible
         resultado[u] = color
 
     for v in range(n):
@@ -42,14 +42,14 @@ if __name__ == '__main__':
     inicio = time.time()
     process = psutil.Process()
 
-    # Fijar una semilla para generar números aleatorios consistentes
+    #Fijar una semilla para generar números aleatorios consistentes
     seed_value = 123
     random.seed(seed_value)
 
-    # Lista de aristas de grafo según el diagrama anterior
+    #Lista de aristas de grafo según el diagrama anterior
     aristas = []
 
-    # Generar vertices de manera aleatoria
+    #Generar vertices de manera aleatoria
     num_vertices = 16 # Número total de vertices
 
     colores = []
@@ -63,13 +63,13 @@ if __name__ == '__main__':
         dest = random.randint(0, num_vertices - 1)
         aristas.append((src, dest))
 
-    # número total de nodos en el grafo
+    #número total de nodos en el grafo
     n = num_vertices
     
-    # construye un grafo a partir de los bordes dados
+    #construye un grafo a partir de los bordes dados
     grafo = Grafo(aristas, n)
 
-    # Grafo de color usando el algoritmo greedy
+    #Grafo de color usando el algoritmo greedy
     colorGrafo(grafo, n)
 
 
