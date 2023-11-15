2a52a9bc93146b65103aa55dbb208e49cf363f99
Matías Mora <128401777+MMforce@users.noreply.github.com>
2023-11-15 19:38:29 -0300

    Update GraphColoring G.py

--- Graph Coloring/GraphColoring G.py
+++ Graph Coloring/GraphColoring G.py
@@ -5,32 +5,32 @@ import random
 import time
 import psutil
 
-# Limitación de 20 nodos
+#Limitación de 20 nodos
 def solve_graph_coloring(graph):
     
-    # Crear el modelo
+    #Crear el modelo
     model = gp.Model('graph_coloring')
 
-    # Variables de decisión
+    #Variables de decisión
     nodes = list(graph.nodes())
     num_colors = len(nodes)
     colors = range(num_colors)
     x = model.addVars(nodes, colors, vtype=GRB.BINARY, name='x')
 
-    # Restricción: cada nodo tiene exactamente un color asignado
+    #Restricción: cada nodo tiene exactamente un color asignado
     model.addConstrs((x.sum(node, '*') == 1 for node in nodes), name='assign_one_color')
 
-    # Restricción: dos nodos adyacentes no pueden tener el mismo color
+    #Restricción: dos nodos adyacentes no pueden tener el mismo color
     model.addConstrs((x[node1, color] + x[node2, color] <= 1
                      for node1, node2 in graph.edges() for color in colors), name='different_colors')
 
-    # Función objetivo: minimizar el número de colores utilizados
+    #Función objetivo: minimizar el número de colores utilizados
     model.setObjective(x.sum(), GRB.MINIMIZE)
 
-    # Resolver el modelo
+    #Resolver el modelo
     model.optimize()
 
-    # Obtener la solución
+    #Obtener la solución
     if model.status == GRB.OPTIMAL:
         solution = {}
         for node in nodes:
@@ -42,27 +42,27 @@ def solve_graph_coloring(graph):
     else:
         return None
 
-# Función base
+#Función base
 if __name__ == '__main__':
     inicio = time.time()
-    # Establecer la semilla aleatoria (replicar modelo)
+    #Establecer la semilla aleatoria (replicar modelo)
     process = psutil.Process()
     random.seed(42)
 
-    # Generan arcos aleatorios, utilizando como referencia el número de vértices
+    #Generan arcos aleatorios, utilizando como referencia el número de vértices
     numero_vertices = 100
     numero_arcos = int((numero_vertices - 1) * numero_vertices / 2)
     graph = nx.gnm_random_graph(numero_vertices, numero_arcos)
 
 
-    # Resolver el problema de coloración de grafos
+    #Resolver el problema de coloración de grafos
     solution = solve_graph_coloring(graph)
 
-    # Calcular el tiempo de ejecución
+    #Calcular el tiempo de ejecución
     termino = time.time()
     tiempoEjecucion = termino - inicio
 
-    # Imprimir el grafo, la solución y el tiempo de ejecución
+    #Imprimir el grafo, la solución y el tiempo de ejecución
     print('Arcos:', graph.edges())
     memory_used = process.memory_info().rss / (1024 * 1024) 
     cpu_usage = psutil.cpu_percent()
