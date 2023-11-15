6f43855c82c052ea9b4d125e3347ae59c4b322a0
Matías Mora <128401777+MMforce@users.noreply.github.com>
2023-11-15 19:39:40 -0300

    Update Knapsack G.py

--- Knapsack/Knapsack G.py
+++ Knapsack/Knapsack G.py
@@ -8,35 +8,35 @@ inicio = time.time()
 process = psutil.Process()
 random.seed(0)
 
-# Número de elementos (objetos)
+#Número de elementos (objetos)
 num_elementos = 90
 
-# Peso máximo permitido
+#Peso máximo permitido
 PESOMAX = 100
 
-# Generar valores y pesos aleatorios para cada elemento
+#Generar valores y pesos aleatorios para cada elemento
 VALOR = {i: random.randint(1, 20) for i in range(1, num_elementos + 1)}
 PESO = {i: random.randint(1, 10) for i in range(1, num_elementos + 1)}
 
-# Conjunto que indica el rango para cada elemento
+#Conjunto que indica el rango para cada elemento
 ELEMENTOS = range(1, num_elementos + 1)
 
-# Crear un nuevo modelo
+#Crear un nuevo modelo
 modelo = gp.Model("Knapsack")
 
-# Crear variables
+#Crear variables
 take = modelo.addVars(ELEMENTOS, vtype=GRB.BINARY, name="Take")
 
-# Establecer la función objetivo que maximiza el valor total de objetos
+#Establecer la función objetivo que maximiza el valor total de objetos
 modelo.setObjective(sum(VALOR[i] * take[i] for i in ELEMENTOS), GRB.MAXIMIZE)
 
-# Establecer la restricción de peso
+#Establecer la restricción de peso
 modelo.addConstr(sum(PESO[i] * take[i] for i in ELEMENTOS) <= PESOMAX)
 
-# Resolver el modelo
+#Resolver el modelo
 modelo.optimize()
 
-# Imprimir el resultado
+#Imprimir el resultado
 if modelo.status == GRB.OPTIMAL:
     print("Solución óptima encontrada:")
     for i in ELEMENTOS:
@@ -45,7 +45,7 @@ if modelo.status == GRB.OPTIMAL:
 else:
     print("No se encontró una solución óptima.")
 
-# Calcular memoria utilizada, uso de CPU y tiempo de ejecución
+#Calcular memoria utilizada, uso de CPU y tiempo de ejecución
 memory_used = process.memory_info().rss / (1024 * 1024)
 cpu_usage = psutil.cpu_percent()
 termino = time.time()
